7291a332cdd7125fd9d0e46ca47e2949a885db7d
cle-ment <cle-ment@gmx.de>
2014-05-30 06:08:46 +0300

    fixed path in build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ dir=`dirname $0`
 FILE_PATH=`cd  $dir;pwd`
 
 echo "building atommapper"
-ATOMMAPPERPATH=$FILE_PATH/examples/keggReactionPrediction/atommapper
+ATOMMAPPERPATH=$FILE_PATH/atommapper
 javac -d $ATOMMAPPERPATH/bin -cp $ATOMMAPPERPATH/src/mapper/ \
 -sourcepath $ATOMMAPPERPATH/src $ATOMMAPPERPATH/src/Mapper2000.java
